ef884825c862be94ee973eac6a4b253e2f07ed6a
Mister2Tone <pisit_tansri@hotmail.com>
2018-02-17 08:09:47 +0700

    sendMessage form textBox to another page

--- app/src/main/java/com/example/android_app/MainActivity.java
+++ app/src/main/java/com/example/android_app/MainActivity.java
@@ -6,10 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.android_app.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void sendMessage(View view){
         Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.text_box);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
